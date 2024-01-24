a98317e3ac9fde5bf26a4366b1b9603c8ec60b67
zackari sl. <zackaria.celimane@gmail.com>
2024-01-24 18:39:26 +0100

    reverting to last working commit

--- src/data/workStore.js
+++ src/data/workStore.js
@@ -2,23 +2,23 @@ let uid = () => crypto.randomUUID().slice(0, 18);
 export const WorkStore = [
 	{
 		id: uid(),
-		name: "CPL Network",
-		position: "Developer",
+		name: "CPLnetwork",
+		position: "Frontend Developer",
 		from: "January 2024",
 		to: "Current",
 		link: "https://www.cpl-network.com/",
 	},
 	{
 		id: uid(),
-		name: "Nuitee - Travel api",
-		position: "Full stack Developer",
+		name: "Nuitee Travel api",
+		position: "Fullstack Developer",
 		from: "October 2022",
 		to: "January 2024",
 		link: "https://nuitee.com",
 	},
 	{
 		id: uid(),
-		name: "AD/HOC - Agency",
+		name: "AD/HOC Agency",
 		position: "Web Developer",
 		from: "May 2022",
 		to: "October 2023.",
@@ -27,7 +27,7 @@ export const WorkStore = [
 	{
 		id: uid(),
 		name: "OMNIYAT",
-		position: "Front-end Developer",
+		position: "Frontend Developer",
 		from: "September 2021",
 		to: "May 2022.",
 		link: "https://www.omniyat.ma/en/",
